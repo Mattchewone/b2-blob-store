1ba7a1aa86458e3f2e7391adf2a123f4ba445ab4
Matt <matt@ideajunction.uk>
2017-07-11 22:53:00 +0100

    Added remove and exists methods

--- index.js
+++ index.js
@@ -16,11 +16,13 @@ function Client(opts = {}) {
   if (!opts.baseDownloadURL) {
     throw new Error('Must specify baseDownloadURL');
   }
-  this.baseURL = opts.baseURL || "https://api.backblaze.com/b2api/v1";
+
+  this.apiUrl = opts.apiUrl || 'https://api.backblaze.com/b2api/v1';
   this.baseDownloadURL = opts.baseDownloadURL;
   this.defaults = {
-   'Authorization': "Bearer " + opts.token
+   'Authorization': opts.token
   };
+  this.token = opts.token;
   this.bucket = opts.bucket;
 }
 
@@ -44,6 +46,59 @@ Client.prototype.request = function(opts, cb) {
   });
 }
 
+Client.prototype.exists = function(opts, cb) {
+  var self = this
+  var bucket = opts.bucket || this.bucket
+
+  if (!bucket) {
+    return proxy.destroy(new Error('Must specify bucket'));
+  }
+  if (!opts.fileName) {
+    return proxy.destroy(new Error('Must specify fileName'));
+  }
+
+  let url = `${this.baseDownloadURL}/file/${bucket}/${opts.fileName}`;
+
+  self.request({ url }, function(err, resp, data) {
+    if (err) {
+      return cb(err);
+    }
+    if (resp.statusCode === 404) {
+      return cb(null, false);
+    }
+    cb(null, !!data);
+  });
+}
+
+Client.prototype.remove = function(opts, cb) {
+  var self = this;
+  var bucket = opts.bucket || this.bucket;
+  if (!bucket) {
+    return cb(new Error('must specify bucket'));
+  }
+  if (!opts.fileId) {
+    return cb(new Error('must specify fileId'));
+  }
+  if (!opts.fileName) {
+    return cb(new Error('must specify fileName'));
+  }
+
+  let fileId = opts.fileId;
+  let fileName = opts.fileName;
+  let url = `${this.apiUrl}/b2api/v1/b2_delete_file_version`;
+
+  self.request({ url, method: 'POST', json: { fileId, fileName } }, function(err, resp, data) {
+    if (err) {
+      return cb(err);
+    }
+    if (resp.statusCode > 299) {
+      console.log(data);
+      return cb(new Error(data));
+    }
+    cb(null);
+  })
+}
+
 Client.prototype.createReadStream = function (opts) {
   let self = this;
   let proxy = duplex();
@@ -56,14 +111,13 @@ Client.prototype.createReadStream = function (opts) {
     return proxy.destroy(new Error('Must specify fileName'));
   }
 
-  let url = this.baseDownloadURL + '/file/' + bucket + '/' + opts.fileName;
+  let url = `${this.baseDownloadURL}/file/${bucket}/${opts.fileName}`;
 
   let read = self.request({ url });
   proxy.setReadable(read);
 
   read.on('response', function(resp) {
     if (resp.statusCode > 299) {
-      console.log(resp);
       proxy.destroy(new Error(resp.statusCode));
     }
   });
